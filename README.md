afff35185ff87d66e58fba58f114c3e68695b874
empty <34548743+tminei@users.noreply.github.com>
2021-02-08 20:52:00 +0200

    Add files via upload

---
+++ README.md
@@ -0,0 +1,27 @@
+# shycopy
+Just copy over ssh. Sugarless. Say 'adzhara gudzhu' to FileZilla!
+
+**For orthodox linux users only!**
+
+Windows users can automate copy using WinSCP or go the fucking out.
+
+## Install
+
+1. Clone.
+```bash
+git clone https://github.com/tminei/shycopy.git
+```
+2. Install system dependencies:
+```bash
+sudo apt install python3 notify-osd sshpass
+```
+3. Install python libs:
+```bash
+pip3 install PyYAML
+```
+
+## Setup
+1. Go to shycopy/projects and copy test.yml to another_file.yml in shycopy/projects folder **dont touch defautl.yml**
+2. Change project key in shycopy/settings.yml
+3. Launch using ```python3 shycopy/main.py```
+4. You can hang on the hotkey ```python3 shycopy/main.py``` to copy automated.
---
+++ main.py
@@ -0,0 +1,66 @@
+import posixpath
+from yaml import load as y_load, dump as y_dump
+from os.path import isfile
+from os import system as run
+
+try:
+    from yaml import CLoader as Loader, CDumper as Dumper
+except ImportError:
+    from yaml import Loader, Dumper
+
+
+def scp(address, port, username, password, local_root, local_path, remote_root, remote_path, ssh_private_key):
+    if not ssh_private_key:
+        scp_cmd = f"sshpass -p '{password}' scp -P {port} -r {local_root}/{local_path} {username}@{address}:{remote_root}/{remote_path}"
+    else:
+        scp_cmd = f"sshpass -p '{password}' scp -t {ssh_private_key} -P {port} -r {local_root}/{local_path} {username}@{address}:{remote_root}/{remote_path}"
+    run(scp_cmd)
+
+
+try:
+    with open("settings.yml", "r") as f:
+        settings = y_load(f.read(), Loader=Loader)
+except:
+    print("\033[91m\033[1m" + "No access to `settings.yml` file!" + "\033[0m")
+    exit(1)
+
+if "project" in settings and isfile("projects/" + settings["project"] + ".yml") or isfile(
+        "projects/" + settings["project"]):
+    project_file = "projects/" + settings["project"]
+    if not project_file.endswith(".yml"):
+        project_file += ".yml"
+else:
+    project_file = "projects/default.yml"
+
+if project_file != "projects/default.yml":
+    try:
+        with open("projects/default.yml", "r") as f:
+            default_project = y_load(f.read(), Loader=Loader)
+    except:
+        print("\033[91m\033[1m" + "No access to `default` project file!" + "\033[0m")
+        exit(1)
+    try:
+        with open(project_file, "r") as f:
+            project = y_load(f.read(), Loader=Loader)
+    except:
+        print(
+            "\033[91m\033[1m" + f"No access to '{project_file.split('/')[-1].split('.')[0]}' project file!" + "\033[0m")
+        exit(1)
+    default_project.update(project)
+    project = default_project
+else:
+    try:
+        with open("projects/default.yml", "r") as f:
+            project = y_load(f.read(), Loader=Loader)
+    except:
+        print("\033[91m\033[1m" + "No access to `default` project file!" + "\033[0m")
+        exit(1)
+
+for i in project["paths"]:
+    i_split = i.split(" ")
+    local_path = i_split[0]
+    remote_path = i_split[1]
+    scp(project["address"], project["port"], project["user"], project["password"], project["local_root"], local_path,
+        project["remote_root"], remote_path, project["ssh_private_key"])
+if settings["notify"]:
+    run(f"notify-send '{project_file.split('/')[-1].split('.')[0]}'")
